1de2a6143d3e45dd19c982b63b2bb0ffb13d9185
ECOcoder23 <eobiozor@luc.edu>
2025-07-15 18:58:11 +0000

    Fifth Draft

--- SuperAles.py
+++ SuperAles.py
@@ -1,22 +1,22 @@
 class SuperAles: 
-    def __init__(self, name: str, abv: float, volume_in_oz: int):
+    def __init__(self, name: str, abv: float):
         self.name = name 
         self.abv = abv 
-        self.volume_in_oz = volume_in_oz
     
-    def alcohol_content(self): 
-        return (self.abv * self.volume_in_oz)
+    def alcohol_content(self, volume_in_oz): 
+        return (self.abv * volume_in_oz)
     
     def description(self): 
         return f"{self.name}: {self.abv*100:.1f}% ABV"
 
-PaleAle = SuperAles("Pale Ale", 0.055, 12)
+PaleAle = SuperAles("Pale Ale", 0.055)
 
-IPA = SuperAles("IPA", 0.065, 12)
+IPA = SuperAles("IPA", 0.065)
 
-Stout = SuperAles("Stout", 0.07, 12)
+Stout = SuperAles("Stout", 0.07)
 
-Porter = SuperAles("Porter", 0.06, 12)
+Porter = SuperAles("Porter", 0.06)
+
 
 
 
