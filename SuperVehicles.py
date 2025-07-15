57963d35af228f77ad39f8be05760175ac80ee94
ECOcoder23 <eobiozor@luc.edu>
2025-07-15 18:53:12 +0000

    Fourth Draft

--- SuperVehicles.py
+++ SuperVehicles.py
@@ -1,3 +1,23 @@
+class SuperVehicles: 
+   def __init__(self, name: str, mpg: int): 
+      self.name = name 
+      self.mpg = mpg
+
+   def fuel_needed(self, distance: float): 
+      return (distance / self.mpg)
+   
+   def description(self): 
+      return f"{self.name} gets {self.mpg} miles per gallon."
+
+Car = SuperVehicles("Car", 30)
+
+Truck = SuperVehicles("Truck", 15)
+
+Motorcycle = SuperVehicles("Motorcycle", 50)
+
+Bus = SuperVehicles("Bus", 8)
+
+
 
 
 #--------------------------------------------------------------------------------#
