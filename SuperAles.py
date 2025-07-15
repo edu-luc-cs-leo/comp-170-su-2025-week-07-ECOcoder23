720981a5f8be4c91383b6cd5ae7db01453dc3221
ECOcoder23 <eobiozor@luc.edu>
2025-07-15 18:12:39 +0000

    First draft

--- SuperAles.py
+++ SuperAles.py
@@ -1,3 +1,30 @@
+class SuperAles: 
+    def __init__(self, name: str, abv: float, volume_in_oz: int):
+        self.name = name 
+        self.abv = abv 
+        self.volume_in_oz = volume_in_oz
+    
+    def alcohol_content(self): 
+        return (self.abv * self.volume_in_oz)
+    
+    def description(self): 
+        return f"{self.name}: {self.abv*100:.1f}% ABV"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #--------------------------------------------------------------------------------#
